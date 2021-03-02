8267e6094aa8d975f37e89c07664ba66d3ee1b0a
ikhsanalatsary <skull.saders18@gmail.com>
2021-03-02 08:10:20 +0700

    work on rn version < 0.63

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react'
-import { NativeMethods } from 'react-native'
+import { NativeComponent } from 'react-native'
 
-export async function measureHeight(component: NativeMethods): Promise<number> {
+export async function measureHeight(
+	component: NativeComponent,
+): Promise<number> {
 	return new Promise(resolve => {
 		component.measure((x, y, w, h) => resolve(h))
 	})
